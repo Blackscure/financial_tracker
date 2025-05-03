f4cee944a6b54ad0c4f06821d2a7fc4a9fe192ec
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-05-03 12:18:29 +0300

    profile given permission

--- finance/api/serializers.py
+++ finance/api/serializers.py
@@ -8,8 +8,16 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ['user']
 
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
+
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaction
         fields = '__all__'
         read_only_fields = ['user']
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
