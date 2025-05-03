f46f2c417e795892e2b8d386b79bf7ba9936ca26
Blackscure <34754398+Blackscure@users.noreply.github.com>
2025-05-03 23:32:31 +0300

    register endpoint consumed

--- finance_tracker/settings.py
+++ finance_tracker/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'knox',
     'django.contrib.sites',
     'authentication',
@@ -43,7 +44,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'finance_tracker.urls'
 
